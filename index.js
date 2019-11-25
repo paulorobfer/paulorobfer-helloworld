2329b15d802cd6dd1843d2e37fb1b4d5fac31e6b
Paulo Ferreira <paulorobfer@gmail.com>
2019-11-25 00:12:05 -0300

    Primeiro Server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000);
+//}).listen(3000, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:3000/');
