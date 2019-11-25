9b8a2bb9fe1918adae861ea26313fb06f8a3adb8
Paulo Ferreira <paulorobfer@gmail.com>
2019-11-25 00:18:06 -0300

    Primeiro Server Heroku

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000);
-//}).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000/');
+// using the http module
+let http = require('http'),
+
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
